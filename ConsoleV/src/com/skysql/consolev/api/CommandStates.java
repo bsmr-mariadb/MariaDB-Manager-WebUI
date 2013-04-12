833230fb400fb616dcb7bc9456cc35ec07094fca
lfei@skysql.com <>
2013-04-12 13:38:37 +0100

    updated copyright with dates

--- ConsoleV/src/com/skysql/consolev/api/CommandStates.java
+++ ConsoleV/src/com/skysql/consolev/api/CommandStates.java
@@ -18,7 +18,7 @@ ould have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Copyright SkySQL Ab
+ * Copyright 2012-2013 SkySQL Ab
  */
 
 package com.skysql.consolev.api;
--- ConsoleV/src/com/skysql/consolev/ui/SettingsDialog.java
+++ ConsoleV/src/com/skysql/consolev/ui/SettingsDialog.java
@@ -18,7 +18,7 @@ SS
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Copyright SkySQL Ab
+ * Copyright 2012-2013 SkySQL Ab
  */
 
 package com.skysql.consolev.ui;
